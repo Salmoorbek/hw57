7e10940c24c1f033e7eeb1ab251e64bf879b4d37
Salmoor <120006078+Salmoorbek@users.noreply.github.com>
2023-04-04 16:35:51 +0600

    Add files via upload
    init commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// let user = { firstName: "Имя", lastName: "Фамилия", email: "example@gmail.com", password: "qwerty", isLoggedIn: false,};
+
+// let post = {
+//     id: 1,
+//     title: "Заголовок поста",
+//     content: "Текст поста",
+//     date: "2023-04-04",
+//     author: user
+// };
+//
+// let comment = {
+//     id: 1,
+//     text: "Ты черт",
+//     date: "2023-04-04",
+//     publication: post
+// };
+//
+// let posts = [];
+// function addPost(title, content, author) {
+//     let post = {title: title, content: content, author: author};
+//     posts.push(post);
+// }
+//
+// addPost("Заголовок поста", "Содержание поста", "Автор поста");
+// console.log(posts);
