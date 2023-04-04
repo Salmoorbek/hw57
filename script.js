e382ded2fc7f86c7454dddaca6ce3f4d0afca4ba
Salmoor <120006078+Salmoorbek@users.noreply.github.com>
2023-04-04 17:14:35 +0600

    Add files via upload
    finished project

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-// let user = { firstName: "Имя", lastName: "Фамилия", email: "example@gmail.com", password: "qwerty", isLoggedIn: false,};
+let user = {
+    firstName: "Имя",
+    lastName: "Фамилия",
+    email: "example@gmail.com",
+    password: "qwerty",
+    isLoggedIn: false,
+};
 
-// let post = {
-//     id: 1,
-//     title: "Заголовок поста",
-//     content: "Текст поста",
-//     date: "2023-04-04",
-//     author: user
-// };
-//
-// let comment = {
-//     id: 1,
-//     text: "Ты черт",
-//     date: "2023-04-04",
-//     publication: post
-// };
-//
-// let posts = [];
-// function addPost(title, content, author) {
-//     let post = {title: title, content: content, author: author};
-//     posts.push(post);
-// }
-//
-// addPost("Заголовок поста", "Содержание поста", "Автор поста");
-// console.log(posts);
+let post = {
+    id: 1,
+    title: "Заголовок поста",
+    content: "Текст поста",
+    date: "2023-04-04",
+    author: user
+};
+
+let comment = {
+    id: 1,
+    text: "Комментарий",
+    date: "2023-04-04",
+    publication: post
+};
+
+let posts = [];
+
+function addPost(post) {
+    posts.push(post);
+}
+
+addPost(post);
+console.log(posts);
+
+function authorizeUser(user) {
+    user.isLoggedIn = true;
+}
+authorizeUser(user)
+console.log(user)
+function toggleLikePost(posts, postId) {
+    const post = posts.find(post => post.id === postId);
+    if (post) {
+        post.liked = !post.liked;
+    }
+}
+
+toggleLikePost(posts, 1)
